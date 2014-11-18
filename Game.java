9db8b0e1a8a37488d0db97a10968408f86b7244b
Emer Thornbury <ethornbury@eircom.net>
2014-11-18 13:27:17 +0000

    First commit

---
+++ Game.java
@@ -0,0 +1,34 @@
+/*
+Game1.java
+@author ET
+@date   Nov 2014
+@desc the instantiable class of rock paper scissors game
+
+*/
+Game{
+	private init computerGuess, personGuess
+	private init rounds
+	
+	play game(){
+		while (round<3){
+			create computerGuess(random){	
+			}
+			compare Guesses(){
+			}
+			edit stats array(){
+			computerLost
+			computerWon
+			personLost
+			personWon
+			
+			}
+			showStats(){
+			print
+			}
+		
+		}
+	
+	
+	}
+
+}
